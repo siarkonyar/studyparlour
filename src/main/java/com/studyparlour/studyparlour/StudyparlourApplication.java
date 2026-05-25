382d25f21b3ba16103a38422dadb5435e0f725f0
siarkonyar <siarkonyar@gmail.com>
2026-05-25 22:21:29 +0100

    first commit

---
+++ src/main/java/com/studyparlour/studyparlour/StudyparlourApplication.java
@@ -0,0 +1,13 @@
+package com.studyparlour.studyparlour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyparlourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyparlourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyparlour/studyparlour/StudyparlourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyparlour.studyparlour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyparlourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
