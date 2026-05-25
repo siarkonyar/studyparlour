8d7186c7d8d16d4faa62055b29291e13a6cca1bc
CaseyDevs <CaseyDevs5@gmail.com>
2026-05-25 23:50:02 +0100

    chore: create initial user model + folder structure

---
+++ src/main/java/com/studyparlour/studyparlour/model/User.java
@@ -0,0 +1,25 @@
+package com.studyparlour.studyparlour.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
