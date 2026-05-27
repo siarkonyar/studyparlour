e5d121ed3ede202492fd2794b43aa4d664d2b5e0
siarkonyar <siarkonyar@gmail.com>
2026-05-27 22:39:57 +0100

    feat: implement user repository

---
+++ src/main/java/com/studyparlour/studyparlour/dataAccess/UserRepository.java
@@ -0,0 +1,11 @@
+package com.studyparlour.studyparlour.dataAccess;
+
+import com.studyparlour.studyparlour.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsUserByEmail(String email);
+}
