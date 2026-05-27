a574afaa09d9f4621e5979a6c78c032131c43209
siarkonyar <siarkonyar@gmail.com>
2026-05-27 23:13:02 +0100

    feat: create user service that implements UserDetailsService. override loadUserByUsername coming forom UserDetailsService

--- src/main/java/com/studyparlour/studyparlour/services/UserService.java
+++ src/main/java/com/studyparlour/studyparlour/services/UserService.java
@@ -1,4 +1,44 @@
 package com.studyparlour.studyparlour.services;
 
-public class UserService {
+import com.studyparlour.studyparlour.dataAccess.UserRepository;
+import com.studyparlour.studyparlour.models.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+//UserDetailsService comes from spring security
+//it is for spring security to look for the user when it creates the token
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found : " + email));
+
+        //java has "var" now apparently
+        var authorities =  user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toSet());
+
+        //we cant import because there is already a user import.
+        //this user is basically the type spring security expect we cant just give our User entity.
+        //it is sorta like mapping.
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+
 }
