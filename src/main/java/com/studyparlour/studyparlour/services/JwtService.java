54a054c665c8aaed37fb903306e3c5871f5c6724
siarkonyar <siarkonyar@gmail.com>
2026-05-28 14:35:08 +0100

    implement generate token and get signing key functions to jwt service

--- src/main/java/com/studyparlour/studyparlour/services/JwtService.java
+++ src/main/java/com/studyparlour/studyparlour/services/JwtService.java
@@ -1,16 +1,38 @@
 package com.studyparlour.studyparlour.services;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Service
 public class JwtService {
 
+    //these two comes from application properties
     @Value("${jwt.secret}")
     private String secret;
 
     @Value("${jwt.expiration-ms}")
     private long expirationMs;
 
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(getSigningKey())
+                .compact();
+    }
 
+
+
+    //this is our key to sign tokens
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
