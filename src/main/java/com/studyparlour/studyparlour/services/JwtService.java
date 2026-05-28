6bbf687e75a8df2395f1ae6aa90d66cb013f3847
siarkonyar <siarkonyar@gmail.com>
2026-05-28 14:42:43 +0100

    feat: extract user properties by token with extractClaim function. use it to verify token

--- src/main/java/com/studyparlour/studyparlour/services/JwtService.java
+++ src/main/java/com/studyparlour/studyparlour/services/JwtService.java
@@ -1,13 +1,16 @@
 package com.studyparlour.studyparlour.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -28,7 +31,28 @@ public class JwtService {
                 .compact();
     }
 
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
 
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String email = extractEmail(token);
+        return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token){
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+    //extract user info from the token
+    public <T> T extractClaim(String token, Function<Claims, T> resolver){
+        Claims claims = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return resolver.apply(claims);
+    }
 
     //this is our key to sign tokens
     private SecretKey getSigningKey() {
