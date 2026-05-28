f0845590147e03cd776e20c4dfb03221d47e4ca2
siarkonyar <siarkonyar@gmail.com>
2026-05-28 16:22:34 +0100

    feat: add rsa key properties config file

--- src/main/java/com/studyparlour/studyparlour/StudyparlourApplication.java
+++ src/main/java/com/studyparlour/studyparlour/StudyparlourApplication.java
@@ -1,9 +1,12 @@
 package com.studyparlour.studyparlour;
 
+import com.studyparlour.studyparlour.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class StudyparlourApplication {
 
 	public static void main(String[] args) {
