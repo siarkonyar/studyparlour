51e8a3f51695bc8f640dc5302553205d2dc2e1a1
siarkonyar <siarkonyar@gmail.com>
2026-07-03 17:34:38 +0300

    feat: add list mapping to study room mapper

--- src/main/java/com/studyparlour/studyparlour/core/mappers/StudyRoomMapper.java
+++ src/main/java/com/studyparlour/studyparlour/core/mappers/StudyRoomMapper.java
@@ -5,10 +5,15 @@ import com.studyparlour.studyparlour.models.StudyRoom;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface StudyRoomMapper {
     //Entity -> Response
     @Mapping(source = "owner.id", target = "ownerId")
     @Mapping(source = "owner.username", target = "ownerUsername")
     StudyRoomResponseDto studyRoomToStudyRoomResponseDto(StudyRoom studyRoom);
+
+    //list mappin does not need @Mapping annotation because it already calls studyRoomToStudyRoomResponseDto for each item.
+    List<StudyRoomResponseDto> studyRoomsToStudyRoomResponseDtos(List<StudyRoom> studyRoomList);
 }
