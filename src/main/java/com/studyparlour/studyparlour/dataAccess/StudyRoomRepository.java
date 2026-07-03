4df2a176bfa549e14c752534246ef9f95d8d4464
siarkonyar <siarkonyar@gmail.com>
2026-07-03 16:01:16 +0300

    feat: create study room respository that extends jpa repository and create fin by owner custom function

--- src/main/java/com/studyparlour/studyparlour/dataAccess/StudyRoomRepository.java
+++ src/main/java/com/studyparlour/studyparlour/dataAccess/StudyRoomRepository.java
@@ -4,8 +4,9 @@ import com.studyparlour.studyparlour.models.StudyRoom;
 import com.studyparlour.studyparlour.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudyRoomRepository extends JpaRepository<StudyRoom, Long> {
-    Optional<StudyRoom> findByOwner(User owner);
+    List<StudyRoom> findByOwner(User owner);
 }
